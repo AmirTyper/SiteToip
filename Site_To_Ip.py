ee1e9ec4fac43d9fe1795274bc0f2a7fc02137ad
Amirtyper <56139421+Amirtyper@users.noreply.github.com>
2019-10-06 18:29:06 +0330

    Host to ip

---
+++ Site_To_Ip.py
@@ -0,0 +1,24 @@
+import socket
+import time
+print ("                 ------------------------------------")
+print ("                 --         Sub Domain Finder      --")
+print ("                 --                 By             --")
+print ("                 --           @Mr_Amir_Typer       --")
+print ("                 --                                --")
+print ("                 ------------------------------------")
+print ("")
+a = socket.gethostbyname(input('[*] Site = ' ))
+time.sleep(4)
+print ('''
+
+[*] Ip Site: 
+
+''')
+time.sleep(2)
+print ("[*]",a)
+time.sleep(8)
+print ('''
+
+[Bye Friend <3 | Script By @Mr_Amir_Typer]
+
+''')
